3c00470261d1b56c5a146ed97c295292ce90349c
tanseer123 <62325889+tanseer123@users.noreply.github.com>
2021-07-08 14:29:40 +0530

    math file

---
+++ math.py
@@ -0,0 +1,18 @@
+#addition
+
+def add(x,y):
+    return a+b
+def subtract(x,y):
+    return a-b
+def multiply(x,y):
+    return a*b #on bug456
+def divide(x,y):
+    if y==0:
+	return DIVIDE_BY_0_ERROR
+    else:
+	return x/y
+#square fn implementation
+def square(x):
+	pass
+
+
